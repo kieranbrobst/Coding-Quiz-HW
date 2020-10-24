062d2e14a9f1567699d883bcf8f2d5b0ee57717f
Kieran Brobst <kieranbrobst@gmail.com>
2020-10-23 21:46:06 -0700

    bootstrap start page and addEventListner

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
+// Variables set //
+var quiz = document.querySelector("#quiz");
 var timerCount = 100;
 var timerEl = document.querySelector("#timer");
+timerEl.textContent = "Time: " + timerCount;
 
-timerEl.textContent = timerCount;
+// Starting Elements before Quiz //
+var questionPrompt = document.createElement("h1");
+questionPrompt.textContent = "Coding Quiz Challenge";
+quiz.appendChild(questionPrompt);
 
-var timer = window.setInterval ( function(){
-    timerCount--;
-    timerEl.textContent = timerCount;
-    if (timerCount === 0){
-        clearInterval(timer)
-    }
-},1000);
+var startBtn = document.createElement("button");
+startBtn.setAttribute("class", "btn btn-info");
+startBtn.textContent = "Start";
+quiz.appendChild(startBtn);
 
 var questions = [
     {
@@ -21,53 +24,24 @@ var questions = [
         choices: ["San Francisco", "Oakland", "Los Angeles"], 
         a: "A"},
 ];
-
 var questionsPointer = 0;
-
 var question = document.querySelector("#question");
 var optionA = document.querySelector("#optionA");
-var optionB = document.querySelector("#optionB");
-var optionC = document.querySelector("#optionC");
 
-function setQuestions() {
-    if (questionsPointer === questions.length){
-        clearInterval(timer);
-        alert("You are done with " + timerCount + " seconds left");
-        return;
-    }
-    question.textContent = questions[questionsPointer].q
-    optionA.textContent = questions[questionsPointer].choices[0];
-    optionB.textContent = questions[questionsPointer].choices[1];
-    optionC.textContent = questions[questionsPointer].choices[2];
 
+// Event Listener Starts the Quiz //
+startBtn.addEventListener("click", function () {
+    startBtn.style.display = "none";
+    var timer = window.setInterval ( function(){
+        timerCount--;
+        timerEl.textContent = "Time: " + timerCount;
+        if (timerCount === 0){
+            clearInterval(timer)
+        }
+    },1000);
+    
 }
 
-setQuestions();
 
-optionA.addEventListener("click", function() {
-    if (optionA.getAttribute("data-answer") === questions[questionsPointer].a) {
-    console.log("this is correct");
-    questionsPointer++;
-    setQuestions()
-} else { 
-    console.log("this is wrong");
-    timerCount -=10;
-}});
-optionB.addEventListener("click", function() {
-    if (optionB.getAttribute("data-answer") === questions[questionsPointer].a) {
-    console.log("this is correct");
-    questionsPointer++;
-    setQuestions()
-} else { 
-    console.log("this is wrong");
-    timerCount -=10;
-}});
-optionC.addEventListener("click", function() {
-    if (optionC.getAttribute("data-answer") === questions[questionsPointer].a) {
-    console.log("this is correct");
-    questionsPointer++;
-    setQuestions()
-} else { 
-    console.log("this is wrong");
-    timerCount -=10;
-}});
+
+)
