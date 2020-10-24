78faac3cb36c913ef8e963d0cb8182a151b6c72e
Kieran Brobst <kieranbrobst@gmail.com>
2020-10-23 22:20:43 -0700

    got most of the javascript quiz structure working

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-// Variables set //
+// Variables //
 var quiz = document.querySelector("#quiz");
 var timerCount = 100;
 var timerEl = document.querySelector("#timer");
 timerEl.textContent = "Time: " + timerCount;
 
-// Starting Elements before Quiz //
 var questionPrompt = document.createElement("h1");
 questionPrompt.textContent = "Coding Quiz Challenge";
 quiz.appendChild(questionPrompt);
@@ -24,10 +23,34 @@ var questions = [
         choices: ["San Francisco", "Oakland", "Los Angeles"], 
         a: "A"},
 ];
-var questionsPointer = 0;
-var question = document.querySelector("#question");
-var optionA = document.querySelector("#optionA");
 
+var questionsPointer = 0;
+var optionA = document.createElement("button");
+optionA.setAttribute("class", "btn btn-info");
+optionA.setAttribute("data-answer", "A");
+var optionB = document.createElement("button");
+optionB.setAttribute("class", "btn btn-info");
+optionB.setAttribute("data-answer", "B");
+var optionC = document.createElement("button");
+optionC.setAttribute("class", "btn btn-info");
+optionC.setAttribute("data-answer", "C");
+
+// Function that sets up the quiz questions // 
+function setQuestions() {
+    quiz.appendChild(optionA);
+    quiz.appendChild(optionB);
+    quiz.appendChild(optionC);
+
+    if (questionsPointer === questions.length){
+        clearInterval(timer);
+        alert("You are done with " + timerCount + " seconds left");
+        return;
+    }
+    questionPrompt.textContent = questions[questionsPointer].q
+    optionA.textContent = questions[questionsPointer].choices[0];
+    optionB.textContent = questions[questionsPointer].choices[1];
+    optionC.textContent = questions[questionsPointer].choices[2];
+}
 
 // Event Listener Starts the Quiz //
 startBtn.addEventListener("click", function () {
@@ -39,9 +62,36 @@ startBtn.addEventListener("click", function () {
             clearInterval(timer)
         }
     },1000);
-    
-}
+    setQuestions();
+    }
+);
 
+// Button or Answer Functions // 
+optionA.addEventListener("click", function() {
+    if (optionA.getAttribute("data-answer") === questions[questionsPointer].a) {
+    console.log("this is correct");
+    questionsPointer++;
+    setQuestions()
+} else { 
+    console.log("this is wrong");
+    timerCount -=10;
+}});
+optionB.addEventListener("click", function() {
+    if (optionB.getAttribute("data-answer") === questions[questionsPointer].a) {
+    console.log("this is correct");
+    questionsPointer++;
+    setQuestions()
+} else { 
+    console.log("this is wrong");
+    timerCount -=10;
+}});
+optionC.addEventListener("click", function() {
+    if (optionC.getAttribute("data-answer") === questions[questionsPointer].a) {
+    console.log("this is correct");
+    questionsPointer++;
+    setQuestions()
+} else { 
+    console.log("this is wrong");
+    timerCount -=10;
+}});
 
-
-)
